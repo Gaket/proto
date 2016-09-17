18ee6278190ed2034f0881efd5fcacf78baabe19
enspa <andrey.leontiev.aiesec@gmail.com>
2016-09-18 00:34:19 +0300

    deal

--- app/src/main/java/ru/innopolis/yorsogettingxbox/deals/DealsActivity.java
+++ app/src/main/java/ru/innopolis/yorsogettingxbox/deals/DealsActivity.java
@@ -3,24 +3,50 @@ package ru.innopolis.yorsogettingxbox.deals;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.innopolis.yorsogettingxbox.R;
+import ru.innopolis.yorsogettingxbox.models.Deal;
 
 public class DealsActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recycler_deals)
+    RecyclerView recyclerDeals;
+    @BindView(R.id.swipe_refresh_deals)
+    SwipeRefreshLayout swipeRefreshDeals;
+    @BindView(R.id.fab_deals)
+    FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deals);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        List<Deal> deals = new ArrayList<Deal>(){
+            {
+                add(new Deal());
+                add(new Deal("Описание.pdf", true, 100));
+            }
+        };
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
