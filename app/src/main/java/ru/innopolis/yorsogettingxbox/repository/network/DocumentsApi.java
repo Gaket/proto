4870701886fd428aad29ebff0391eb89748d483c
Artur <artusletter@gmail.com>
2016-09-18 04:31:48 +0300

    Send file

--- app/src/main/java/ru/innopolis/yorsogettingxbox/repository/network/DocumentsApi.java
+++ app/src/main/java/ru/innopolis/yorsogettingxbox/repository/network/DocumentsApi.java
@@ -3,14 +3,13 @@ package ru.innopolis.yorsogettingxbox.repository.network;
 import java.util.List;
 
 import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Path;
 import ru.innopolis.yorsogettingxbox.models.Document;
+import ru.innopolis.yorsogettingxbox.models.DocumentsResponse;
 import rx.Observable;
 
 public interface DocumentsApi {
@@ -19,7 +18,5 @@ public interface DocumentsApi {
 
     @Multipart
     @POST("deals/{id}/documents")
-    Call<ResponseBody> upload(int id,
-                              @Part("description") RequestBody description,
-                              @Part MultipartBody.Part file);
+    Observable<DocumentsResponse> upload(@Path("id") int id, @Part MultipartBody.Part file);
 }
